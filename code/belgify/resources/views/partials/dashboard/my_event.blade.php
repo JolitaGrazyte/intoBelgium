1938840ba947e72651c471de9a0f5d4c77ae87ef
vicdenys <vicdenys@hotmail.com>
2015-11-09 11:37:02 +0100

    event wat mooier maken

--- code/belgify/resources/views/partials/dashboard/my_event.blade.php
+++ code/belgify/resources/views/partials/dashboard/my_event.blade.php
@@ -1,47 +1,25 @@
 <div class="row d-event">
-    <div class="col-md-3 events-dates">
-
-        <div class="border-right">
+    <div class="col-md-2 d-event-dates">
 
             <div class="day">{{ $event->date->day }}</div>
             <div class="month">{{ $event->date->format('F') }}</div>
             <div class="year">{{ $event->date->year }}</div>
 
-        </div>
-
     </div>
 
     <div class="col-md-10 d-event-detail">
-        <h2> {{ $event->title }}</h2>
+            <h2> {{ $event->title }}</h2>
 
-        {{--TODO: remove inline styles--}}
-        <em style="color: darkgray">{{ $event->created_at->toDayDateTimeString() }}</em>
-        <p> {{ $event->description }} </p>
+            <div class="place-hour">
+                <p class="city"> {{ $event->location->name }} </p>
+                <p>&middot;</p>
+                <p> starts at {{ $event->date->format('H:i') }} </p>
+            </div>
 
-        <div class="place-hour">
-            <p class="city"> {{ $event->location->name }} </p>
-            <p>&middot;</p>
-            <p> starts at {{ $event->date->format('H:i') }} </p>
-        </div>
+            <p class="attenders"> {{ $event->attenders->count()}} participants</p>
 
-        <p class="attenders"> {{ $event->attenders->count()}} participants</p>
+            <p> Posted by: {{ $event->author->username }}, <em>{{ $event->created_at->diffforHumans() }}</em> </p>
 
-        <p> Posted by: {{ $event->author->username }}, <em>{{ $event->created_at->diffforHumans() }}</em> </p>
-
-        {!!Form::open(['route' => ['follow', $event->author->id], 'class' => 'form-horizontal', 'role' => 'form'])  !!}
-
-        <div class="form-group">
-
-            {{--TODO: Remove method to a better place--}}
-
-            {!! Form::hidden('follow', Auth::user()->userIsFollowing(Auth::user()->id, $event->author->id)?0:1, ['class' => '', 'onchange' => 'this.form.submit())']) !!}
-
-            {!! Form::submit(Auth::user()->userIsFollowing(Auth::user()->id, $event->author->id)?'Following':'Follow', ['class' => !Auth::user()->userIsFollowing(Auth::user()->id, $event->author->id)?'btn-follow btn btn-primary':'btn btn-primary ']) !!}
 
         </div>
-
-        {!!Form::close() !!}
-
-
-    </div>
 </div>
