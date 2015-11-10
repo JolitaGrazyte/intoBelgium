d3994f864db0b0ec9064be62ded24483ec4779d8
Jolita Grazyte <jolita.grazyte@student.kdg.be>
2015-11-10 17:06:41 +0100

    search index, linking from tags

--- code/belgify/resources/views/search/index.blade.php
+++ code/belgify/resources/views/search/index.blade.php
@@ -10,11 +10,45 @@
 
         <div class="row">
 
-            @if(isset($search_results))
 
-                @each('search.single', $search_results, 'search_result', 'search.no-results')
+{{--                @if(count($search_results['tours']))--}}
 
+                    @if(count($search_results))
+
+                    {{--@foreach($search_results as $search_result)--}}
+
+                        @each('search.single', $search_results, 'result')
+
+                    {{--@endforeach--}}
+
+                {{--@foreach($tours as $tour)--}}
+
+                    {{--<div class="col-md-10">--}}
+
+                        {{--<hr>--}}
+
+                        {{--<h3> {{ $tour->title }}</h3>--}}
+                        {{--<p> {{ $tour->description }}</p>--}}
+
+                    {{--</div>--}}
+
+                {{--@endforeach--}}
             @endif
+
+
+                    {{--@each('search.single', $search_results['tours'], 'result', 'search.no-results')--}}
+
+                {{--@endif--}}
+
+
+
+                {{--@if(count($search_results['questions']))--}}
+
+                    {{--@each('search.single', $search_results['questions'], 'result', 'search.no-results')--}}
+
+
+                {{--@endif--}}
+
 
         </div>
     </div>
