3b5111e41a92d4f2500fd4483a1cd5ea88c42faf
Jolita Grazyte <jolita.grazyte@student.kdg.be>
2015-11-15 12:10:47 +0100

    delete confirm events

--- code/belgify/app/Http/Controllers/CommentsController.php
+++ code/belgify/app/Http/Controllers/CommentsController.php
@@ -7,7 +7,6 @@ use App\Tag;
 use App\Comment;
 use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Votes;
 use Session;
