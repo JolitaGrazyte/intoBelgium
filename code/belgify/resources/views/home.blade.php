118510ee2673966fcd5efa7b62eda09dd2f9dab6
vicdenys <vicdenys@hotmail.com>
2015-11-15 22:03:32 +0100

    homepage

--- code/belgify/resources/views/home.blade.php
+++ code/belgify/resources/views/home.blade.php
@@ -8,21 +8,31 @@
 
 
 
+    <div class="container-fluid home-container">
+        <div class="background-skew"></div>
 
+        <div class="home_header">
 
-    <div class="home_header">
+            <div class="container">
 
-        <div class="container">
+                @include('partials.search')
 
-            <h3 class="quote">“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua”</h3>
+            </div>
 
-            @include('partials.search')
 
         </div>
 
 
+        <div class="home-content">
+            <div class="about">
+                <img src="/img/Logo.png" alt="Logo into B"/>
+                <p>into Belgium is a social integration platform that tries to connect people - the newcomers with locals.
+                    If you are new in Belgium and feel like you could use some help of locals you are at the right place. Go search for an answer to your question. You might find the answer here. You might join one or another event organized by locals to find out more about everyday life in Belgium.
+                    If you are local, socially engaged and willing to help the newcomers to find their way in Belgium you can do it here. Your help is needed and valued.
+                </p>
+            </div>
+        </div>
     </div>
-
 
 
 @stop
